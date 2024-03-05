67ac71ff58b3c068457886a3db2e2903c473a246
Fromb23 <rombo.programming@gmail.com>
2024-03-05 05:08:33 -0800

    task 3 of BaseModel mode

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
 
-class BaseModel:
+import uuid
+from datetime import datetime
 
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[<{}>] (<{}>) <{}>".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = type(self).__name__
+
+        # Convert datetime objects to ISO format strings
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        
+        return dict_copy
