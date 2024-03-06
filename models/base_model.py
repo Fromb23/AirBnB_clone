51f9904dbb4e1954d39323004a18a05c5e9fb8ff
Edith Mokeira <edithmokeira7@gmail.com>
2024-03-07 00:48:02 +0300

    Added: Doctrings for better readability

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,24 @@ from models.engine.file_storage import FileStorage
 from models import storage
 
 class BaseModel:
+    """
+    Base class for other classes to inherit common attributes and methods.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Attributes:
+            id (str): Unique identifier for the instance.
+            created_at (datetime): Timestamp representing creation time.
+            updated_at (datetime): Timestamp representing the last update time.
+        """
+
         self.id = str(uuid.uuid4())
         if "created_at" in kwargs:
             self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
@@ -27,12 +44,28 @@ class BaseModel:
 
 
     def __str__(self):
+        """
+        Returns (str) string representation of the instance.
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
+        """
+        Saves the current instance to the storage.
+        
+        Updates the 'updated_at' attribute and calls the storage save method.
+        """
         self.updated_at = datetime.now()
         storage.save()
 
+
     def to_dict(self):
+        """
+        Converts the instance to a dictionary representation.
+
+        Returns:
+            dict: Dictionary representation of the instance.
+        """
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = type(self).__name__
 
