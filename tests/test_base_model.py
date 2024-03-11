2d7119d850d6464a98f51d2e4b9db83083abaea6
Edith Mokeira <edithmokeira7@gmail.com>
2024-03-11 23:48:59 +0300

    Added: Modified test file for the BaseModel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -9,13 +9,16 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Test cases for the 'BaseModel' class."""
+
     def test_init(self):
+        """Test the initialization of BaseModel without arguments."""
         obj = BaseModel()
         self.assertIsNotNone(obj.id)
         self.assertIsInstance(obj.created_at, datetime)
         self.assertIsInstance(obj.updated_at, datetime)
 
     def test_init_with_args(self):
+        """Test the initialization of BaseModel with arguments."""
         obj = BaseModel(id="123", created_at="2022-01-01T12:00:00.000000", name="test")
         self.assertEqual(obj.id, "123")
         self.assertEqual(obj.name, "test")
@@ -23,11 +26,13 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(obj.updated_at, datetime)
 
     def test_str(self):
+        """Test the string representation of BaseModel."""
         obj = BaseModel(id="123", created_at="2022-01-01T12:00:00.000000", name="test")
         expected_str = "[{}] ({}) {}".format(obj.__class__.__name__, obj.id, obj.__dict__)
         self.assertEqual(str(obj), expected_str)
 
     def test_to_dict(self):
+        """Test the to_dict method of BaseModel."""
         instance = BaseModel()
         instance.created_at = datetime(2024, 3, 11, 12, 0, 0)
         instance.updated_at = datetime(2024, 3, 12, 12, 0, 0)
