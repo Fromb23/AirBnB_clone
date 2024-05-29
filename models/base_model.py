da3c0009aba667e7681a51daa6a0e4cde0864ec1
Fromb23 <rombo.programming@gmail.com>
2024-05-29 13:32:57 +0300

    updated basemodel with args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,19 @@ import datetime
 
 class BaseModel():
     """Base model class for other models."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
 
     def __str__(self):
         """Return string representation of BaseModel object."""
